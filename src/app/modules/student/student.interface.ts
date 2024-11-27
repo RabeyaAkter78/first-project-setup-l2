aef2998daf0fdc8998e9c33e518faaf5ec59df6e
RabeyaAkter78 <rabeyariya251@gmail.com>
2024-11-27 23:08:19 +0600

    create schema

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,37 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+export type localGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+export type userName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+export type student = {
+  id: string;
+  name: userName;
+  gender: 'male| female';
+  email: string;
+  dateOfBirth?: student;
+  contactNumber: string;
+  emergencyCOntactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: guardian;
+  localGuardian: localGuardian;
+  profileImg?: string;
+  isActive: 'active|blocked';
+};
