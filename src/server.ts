9b12c0cf01e0c96843f8a5e82703ed07b41610db
Rabeya Akter <rabeyariya251@gmail.com>
2024-12-03 18:42:42 +0600

    chnage port and connect mongodb

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,9 @@ main().catch(err => console.log(err));
 async function main() {
 
     try {
-        await mongoose.connect(Config.database_url as string);
+        await mongoose.connect('mongodb://localhost:27017/' as string);
 
-        app.listen(Config.port, () => {
+        app.listen(5002, () => {
             console.log(`Example app listening on port ${Config.port}`)
         })
     } catch (error) {
@@ -18,6 +18,6 @@ async function main() {
     }
 }
 
-main();
+
 
 
